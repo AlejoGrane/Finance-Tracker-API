fa71f84b484ecef0bdd88a978fdfdb6b862ae5bb
Alejo Grané <alejograne@gmail.com>
2026-08-19 09:31:34 -0300

    Estructura Inicial Expense Tracker API

---
+++ src/config/db.js
@@ -0,0 +1,14 @@
+const MYSQL = require(`mysql2/promise`);
+require(`dotenv`).config();
+
+const POOL = MYSQL.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+module.exports = POOL;
